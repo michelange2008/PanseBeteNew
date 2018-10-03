cc6548d8e58182f133e404ce297992b6b0f66072
michelange2008 <michelange@wanadoo.fr>
2018-10-03 17:36:08 +0200

    petit commit pour tester le remote

--- core/resources/views/menus/sousmenu.blade.php
+++ core/resources/views/menus/sousmenu.blade.php
@@ -1,10 +1,14 @@
 @section('sousmenu')
 @if(session()->get('espece'))
-  <div class="espece-item bg-light d-flex flex-row">
-    <img src="{{URL::asset('svg/especes')."/".session()->get('espece')->icone}}" alt="{{session()->get('espece')->nom}}" />
-    <h5 class="btn -btn-light btn-inactiv">{{session()->get('espece')->nom}}</h5>
-    <a href="{{URL::route('accueil')}}"><img src="{{URL::asset('/svg')}}/home.svg" alt="accueil" title="accueil" /></a>
-    <a href="{{URL::previous()}}"><img src="{{URL::asset('/svg')}}/retour.svg" alt="retour" title="page précédente" /></a>
+  <div class="espece-item bg-light d-flex flex-row justify-content-between">
+    <div>
+      <a href="{{route('accueil')}}"><img src="{{URL::asset('svg/especes')."/".session()->get('espece')->icone}}" alt="{{session()->get('espece')->nom}}" /></a>
+      <h5 class="btn -btn-light btn-inactiv">{{session()->get('espece')->nom}}</h5>
+    </div>
+    <div class="d-flex flex-column justify-content-between">
+      <a class="lien-nav" href="{{URL::route('accueil')}}"><img class = "icone-nav" src="{{URL::asset('/svg')}}/home.svg" alt="accueil" title="accueil" /></a>
+      <a class="lien-nav" href="{{URL::previous()}}"><img class = "icone-nav" src="{{URL::asset('/svg')}}/retour.svg" alt="retour" title="page précédente" /></a>
+    </div>
   </div>
 @else()
   <div class="espece-item bg-light d-flex flex-row">
