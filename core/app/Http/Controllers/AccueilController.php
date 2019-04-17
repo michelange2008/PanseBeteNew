0d0ac732771f618279cbf93253878c6d2438649d
pansebetbj <pansebetbj@ssh01.cluster027.gra.hosting.ovh.net>
2019-04-17 06:32:55 +0000

    je ne sais pas sur AccueilController ?

--- core/app/Http/Controllers/AccueilController.php
+++ core/app/Http/Controllers/AccueilController.php
@@ -17,7 +17,7 @@ class AccueilController extends Controller
 
     public function index()
     {
-      return view('front');
+      return view('front_avant');
     }
 
     public function accueil()
