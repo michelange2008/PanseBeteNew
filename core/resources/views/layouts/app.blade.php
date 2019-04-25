486152c271219169361acc7cf5c0eea9aed0e0c5
michelange2008 <michelange@wanadoo.fr>
2019-04-25 17:45:41 +0200

    ajout du script matomo

--- core/resources/views/layouts/app.blade.php
+++ core/resources/views/layouts/app.blade.php
@@ -16,6 +16,22 @@
 
     <link rel="stylesheet" href="{{asset(config('chemins.css'))}}/app.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css" />
+
+    <!-- Matomo -->
+      <script type="text/javascript">
+        var _paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="//panse-betes.fr/matomo/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '1']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+      </script>
+    <!-- End Matomo Code -->
 </head>
 <body>
 
