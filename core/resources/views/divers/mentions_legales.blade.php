c05b41a63b17d94848a1f2f8792332bec6e8a73c
michelange2008 <michelange@wanadoo.fr>
2019-05-08 12:21:44 +0200

    modif avertissement suivi 1

--- core/resources/views/divers/mentions_legales.blade.php
+++ core/resources/views/divers/mentions_legales.blade.php
@@ -4,16 +4,24 @@
 
 @section('contenu')
 
-  <div class="alert alert-success rounded-0">
-    <h1>Mentions légales</h1>
-
-    <iframe
-
-        style="border: 0; height: 200px; width: 600px;"
-
-        src="https://panse-betes.fr/matomo/index.php?module=CoreAdminHome&action=optOut&language=fr&backgroundColor=efeded&fontColor=&fontSize=15px&fontFamily=PT%20Sans">
-
-      </iframe>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="alert alert-success rounded-0">
+        <h1>Mentions légales</h1>
+      </div>
+    </div>
   </div>
+  <div class="row">
+    <div class="col-md-10">
+      <iframe
+        style="border: 0; height: 200px; width: 600px;"
+        src="https://panse-betes.fr/matomo/index.php?module=CoreAdminHome&action=optOut&language=fr&backgroundColor=efeded&fontColor=&fontSize=15px&fontFamily=PT%20Sans">
+      </iframe>
+    </div>
+  </div>
+</div>
+
+
 
 @endsection
