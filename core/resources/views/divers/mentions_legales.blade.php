d5d6382f4add31d1709875eec2ded61bebe465e6
michelange2008 <michelange@wanadoo.fr>
2019-05-08 12:31:08 +0200

    suivi 2

--- core/resources/views/divers/mentions_legales.blade.php
+++ core/resources/views/divers/mentions_legales.blade.php
@@ -14,8 +14,13 @@
   </div>
   <div class="row">
     <div class="col-md-10">
+      <h6>Suivi de la fréquentation du site</h6>
+      <p>Nous avons mis en place un outil de suivi de la fréquentation de Panse-Bêtes afin de connaître l'utilisation de cet outil.</p>
+      <p>Il s'agit d'un logiciel libre, nommé Matomo, qui non intrusif, qui anonymise votre adresse IP et respecte la réglementation française</p>
+      <p>Pour plus d'information, vous pouvez consulter le site de la CNIL <a href="https://www.cnil.fr/fr/solutions-pour-les-cookies-de-mesure-daudience" title="Site de la CNIL"><i class="fas fa-external-link-square-alt"></i></a> </p>
+      <p>Il vous est possible de décider de ne pas être suvi. Il vous suffit de décocher la case ci-dessous</p>
       <iframe
-        style="border: 0; height: 200px; width: 600px;"
+        style="border: 0; height: 200px; width: 100%;"
         src="https://panse-betes.fr/matomo/index.php?module=CoreAdminHome&action=optOut&language=fr&backgroundColor=efeded&fontColor=&fontSize=15px&fontFamily=PT%20Sans">
       </iframe>
     </div>
