bd762359ba0a632f8eeda9dbbc6d5f783e1c4b65
acsasat <acsasat@ssh01.cluster030.gra.hosting.ovh.net>
2022-07-23 17:14:52 +0000

    premier commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,23 +18,23 @@ class DatabaseSeeder extends Seeder {
     $this->call(RoleTableSeeder::class);
     $this->call(EspecesTableSeeder::class);
     $this->call(TypesTableSeeder::class);
-    $this->call(ElevagesTableSeeder::class);
+#    $this->call(ElevagesTableSeeder::class);
     $this->call(ParafermesTableSeeder::class);
     $this->call(AlertesTableSeeder::class);
-    $this->call(UsersTableSeeder::class);
+#    $this->call(UsersTableSeeder::class);
     $this->call(ChiffresTableSeeder::class);
     $this->call(CritalertesTableSeeder::class);
-    $this->call(NotesTableSeeder::class);
+#    $this->call(NotesTableSeeder::class);
     $this->call(NumalertesTableSeeder::class);
     $this->call(OriginesTableSeeder::class);
-    $this->call(SaisiesTableSeeder::class);
-    $this->call(SalertesTableSeeder::class);
-    $this->call(SchiffresTableSeeder::class);
-    $this->call(SindicateursTableSeeder::class);
-    $this->call(SoriginesTableSeeder::class);
+#    $this->call(SaisiesTableSeeder::class);
+#    $this->call(SalertesTableSeeder::class);
+#    $this->call(SchiffresTableSeeder::class);
+#    $this->call(SindicateursTableSeeder::class);
+#    $this->call(SoriginesTableSeeder::class);
     // $this->call(AmisTableSeeder::class);
-    $this->call(EspeceNoteTableSeeder::class);
+#    $this->call(EspeceNoteTableSeeder::class);
     $this->call(EspeceParticipantTableSeeder::class);
-    $this->call(ParafermeUserTableSeeder::class);
+#    $this->call(ParafermeUserTableSeeder::class);
     }
 }
