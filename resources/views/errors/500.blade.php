813bf90c21add141ddf53176c23bf5c67601b765
michelange2008 <michelange@wanadoo.fr>
2022-07-25 19:10:32 +0200

    amélioration des messages d'erreur

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,11 +3,6 @@
 
 @section('contenu')
 
-<div class="jumbotron">
-  <h1 class="display-4">Désolé !</h1>
-  <p class="lead">Il semble que vous vous soyez égarés dans les dédales de l'application</p>
-  <hr class="my-4">
-  <p>Mais n'ayez crainte, il vous suffira de cliquer sur le bouton ci-dessous pour trouver une issue.</p>
-  <a class="btn btn-otobleu btn-lg" href="{{route('accueil')}}" role="button">Sortir</a>
-</div>
+  @include('errors.error_tpl')
+
 @endsection
