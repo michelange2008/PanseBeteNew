c7bf84c9ad5d21e2a0d01904b2f2a3d2ab1ad01e
michelange2008 <michelange@wanadoo.fr>
2022-09-05 06:54:47 +0200

    seeder chiffreespece

---
+++ database/seeders/ChiffreEspeceTableSeeder.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ChiffreEspeceTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      \DB::table('chiffre_espece')->insert(array (
+
+        [ 'chiffre_id' => 1, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 2, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 3, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 4, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 5, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 6, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 7, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 8, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 9, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 10, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 11, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 12, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 13, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 14, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 15, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 16, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 17, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 18, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 19, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 20, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 21, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 22, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 23, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 24, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 25, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 26, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 27, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 28, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 29, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 30, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 31, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 32, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 33, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 34, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 35, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 36, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 37, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 38, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 39, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 40, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 41, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 42, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 43, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 44, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 45, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 46, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 47, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 48, 'espece_id' => 2, ],
+        [ 'chiffre_id' => 59, 'espece_id' => 2, ],
+
+        )
+      );
+    }
+}
