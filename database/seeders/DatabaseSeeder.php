99c711789efc6b56d2b85f28fd2441ec30e96449
michelange2008 <michelange@wanadoo.fr>
2022-11-03 07:03:56 +0100

    Modif seeder typenums

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,6 +37,7 @@ class DatabaseSeeder extends Seeder {
     // $this->call(EspeceParticipantTableSeeder::class);
     // $this->call(ParafermeUserTableSeeder::class);
     // $this->call(ChiffreEspeceTableSeeder::class);
+    // $this->call(TypenumsTableSeeder::class);
 
     }
 }
--- database/seeders/TypenumsTableSeeder.php
+++ database/seeders/TypenumsTableSeeder.php
@@ -20,25 +20,25 @@ class TypenumsTableSeeder extends Seeder
 
         [
           'nom' => "entier",
-          'detail' => "(1, 20, 32)",
+          'detail' => "1 - 20 - 32",
           'step' => 1,
           'arrondi' => 0,
         ],
         [
           'nom' => "décimal avec 1 chiffre après la virgule",
-          'detail' => "(0,1 - 1,4 - 10,3)",
+          'detail' => "0,1 - 1,4 - 10,3",
           'step' => 0.1,
           'arrondi' => 1,
         ],
         [
           'nom' => "décimal avec 2 chiffres après la virgule",
-          'detail' => "(1,34 - 20,50 - 32,12)",
+          'detail' => "1,34 - 20,50 - 32,12",
           'step' => 0.01,
           'arrondi' => 2,
         ],
         [
           'nom' => "décimal avec 3 chiffres après la virgule",
-          'detail' => "(1,234)",
+          'detail' => "1,234, 24,498",
           'step' => 0.001,
           'arrondi' => 3,
         ],
